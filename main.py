ac3a19dec2f15e0d95fba3a607ec34e49ac9e44f
Pizza-py <140508494+pizza-py@users.noreply.github.com>
2024-08-05 16:57:27 +0100

    Finished implementing Matrix Calculator
    Toyed around with a coordinate subclass before commenting it out to avoid overcomplication
    
    Maybe come back to it?

--- main.py
+++ main.py
@@ -1,34 +1,69 @@
 import math
 
+
 class Matrix:
     def __init__(self, contents: list):
+        #Verification
+        try:
+            if type(contents) != list:
+                raise Exception
+            presumedLength = len(contents[0])
+
+            for i in range(len(contents)):
+                if len(contents[i]) != presumedLength:
+                    raise Exception
+                elif type(contents[i]) != list:
+                    raise Exception
+        except:
+            raise Exception("Invalid Matrix")
+
         self.height = len(contents)
         self.length = len(contents[0])
         self.contents = contents
-    def __len__(self) -> str:
-        return str(self.length)+"x"+str(self.height)
+        self.dimensions = str(self.height)+"x"+str(self.length)
+
+
+    def __add__(self,mat2):
+        if self.dimensions == mat2.dimensions:
+            return Matrix([[self.contents[i][j] + mat2.contents[i][j] for j in range(self.length)] for i in range(self.height)]) 
+        else: 
+            return 
+
+    def __mul__(self,obj):
+        if type(obj) == int:
+            return Matrix([[self.contents[i][j] * obj for j in range(self.length)] for i in range(self.height)])
+        
+        elif issubclass(type(obj),Matrix):
+            if self.length == obj.height:
+                resultMat = []
+                for i in range(self.height):
+                    resultMat.append([])
+                    for j in range(obj.length):
+                        total = 0
+                        for k in range(self.length): #Can Be either self.length or obj.height
+                            total += self.contents[i][k] * obj.contents[k][j]
+                        resultMat[i].append(total)
+                return Matrix(resultMat)
+            else:
+                return
+            
+    def __rmul__(self,obj):
+        if type(obj) == int:
+            return Matrix([[self.contents[i][j] * obj for j in range(self.length)] for i in range(self.height)])
+        
+    def __str__(self):
+        return str(self.contents)
     
-def MatMul(mat1: Matrix, mat2: Matrix) -> Matrix:
-    if mat1.length != mat2.height:
-        return
-    else:
-        return
-def validateMatrix(mat1):
-    try:
-        if type(mat1) != list:
-            return False
+    @classmethod
+    def fromCoordinates(cls, contents):
+        return cls([[i] for i in contents])
 
-        presumedLength = len(mat1[0])
- 
-        for i in range(len(mat1)):
-            if len(mat1[i]) != presumedLength:
-                return False
-            elif type(mat1[i]) != list:
-                return False
-    except:
-        return False
-    return True
-
-matrix = [1,1,1]
-print(validateMatrix(matrix))
-
+'''
+class Coordinate(Matrix):
+    def __init__(self, contents: list):
+        for element in contents:
+            print(type(element))
+            if type(element) != int and type(element) != float:
+                raise Exception("Invalid Coordinate")
+        super().__init__([[i] for i in contents])
+'''
