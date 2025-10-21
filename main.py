9b938d628f362ce496958e4bdb163bc692563004
David <tanidt2@outlook.com>
2025-10-21 19:16:24 +0100

    Completely forgot to update the Github
    Wow, I was working on this locally this entire time

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from math import sin,cos
+#Imports
+
+from math import sin,cos,tan, pi,e
 import pygame
 import sys 
 
+#Constants
+
+width, height = 800, 600
+black = 0,0,0  #Values for pygame BLACK
+white = 255,255,255  #Values for pygame WHITE
+rotationsPerWidth = 3  #Measure of Sensitivity - how many times the graph rotates when you move the mouse from the far left to the far right (politics har har)
+axisLength = (0.4 * min([width,height]))
+graphResolution = 5  #The detail of the actual graph - The smaller this number the higher the resolution (and also the lag)
+circleResolution = 50 #The detail of the circles which bound the ends of the shape - The higher this number the higher the resolution (yeah it's confusing but whatever)
+numberOfRevolutions = 10  #How many revolution lines are rendered
+axisWidth = 2
+
 class Matrix:
+    '''
+    Matrix module: This is where the magic happens. Every rotation is calculated as the product of the vector matrix and a rotation matrix, but I added a few other operations as well
+    '''
     def __init__(self, contents: list):
-        try:
+        try: #Data validation so boring
             if type(contents) != list:
                 raise Exception
             presumedLength = len(contents[0])
@@ -22,13 +39,16 @@ class Matrix:
         self.contents = contents
         self.dimensions = str(self.height)+"x"+str(self.length)
 
+    #Takes two matrices and adds them together
     def __add__(self,mat2):
         if self.dimensions == mat2.dimensions:
             return type(self)([[self.contents[i][j] + mat2.contents[i][j] for j in range(self.length)] for i in range(self.height)]) 
         else: 
             return 
+        
 
-    def __mul__(self,obj):
+    #Takes two matrices and multiplies them together
+    def __mul__(self,obj): 
         if type(obj) == int:
             return type(self)([[self.contents[i][j] * obj for j in range(self.length)] for i in range(self.height)])
         
@@ -46,13 +66,14 @@ class Matrix:
             else:
                 return
             
-    def __rmul__(self,obj):
+    def __rmul__(self,obj): #Just in case I want to write integer * matrix instead of matrix * integer
         if type(obj) == int:
             return type(self)([[self.contents[i][j] * obj for j in range(self.length)] for i in range(self.height)])
         
-    def __str__(self):
+    def __str__(self): #Debugging purposes
         return str(self.contents)
     
+    #This is important - it returns a fully geared rotation matrix from a bank of "templates" of important matrices.
     @classmethod
     def rotationMatrix(cls, angle: float,axis: str):
         matrices = {
@@ -62,8 +83,8 @@ class Matrix:
             ]),
 
             "clockwise" : Matrix([
-                [cos(angle),-sin(angle)],
-                [sin(angle),cos(angle)]
+                [cos(angle),sin(angle)],
+                [-sin(angle),cos(angle)]
             ]),
 
             "x" : Matrix([
@@ -88,88 +109,159 @@ class Matrix:
 
 class Vector(Matrix):
 
+    '''
+    Vector class: I wanted something different to distinguish between matrices and vectors (essentially coordinates) because they need a few extra properties
+
+    Vectors are a subclass of Matrices so they inherit all of its properties and functions
+    '''
     def __init__(self, contents: list):
+        if type(contents) == Vector: #More data validation
+            contents = contents.vectorContents
         for element in contents:
             if type(element) != int and type(element) != float and type(element) != list:
                 raise Exception("Invalid Vector")
             elif type(element) == list:
                 if len(element) != 1:
                     raise Exception("Invalid Vector")
-        if type(contents[0]) == int:
+        if type(contents[0]) == int or type(contents[0]) == float:
             super().__init__([[i] for i in contents])
         else: 
             super().__init__(contents)
         self.vectorContents = [i[0] for i in self.contents] 
     
-    def rotate(self,angle,axis):
+    def rotate(self,angle,axis): #Takes a point and rotates it
         return Matrix.rotationMatrix(angle,axis)*self
-
-class Vector3(Vector):
-    def __init__(self, contents: list):
-        super().__init__(contents)
-    def project(self):
-        return Vector2([self.vectorContents[i] for i in range(len(self.vectorContents)-1)])
     
-class Vector2(Vector):
-    def __init__(self, contents: list):
-        super().__init__(contents)
-
     @property
-    def toPygame(self):
-        mat = [
-            height/2,
-            width/2
-        ]
-        return self+mat
+    def projectToPygame(self): #Takes a multidimensional vector and projects it to 2D
+        vec2 = Vector([self.vectorContents[0], -self.vectorContents[1]])
+        return (vec2+Vector([width / 2,height / 2])).vectorContents
 
 
-class Circle:
-    def __init__(self,coords):
-        self.coords = coords
+class obj:
+    '''
+    Obj class: One last class for handling objects such as the axis or the graph itself
 
-circles = []
+    An obj is a collection of lines in order, and lines themselves are just a double of 2 points (I couldn't be asked to make a line class)
+    '''
+    objects = []  #An important record of all the object instances
+    def __init__(self, lines, lineInfo = {}): #I actually used kwargs for once
+        self.lines = [[Vector(point) for point in line] for line in lines]
+        self.lineInfo = lineInfo
+        obj.objects.append(self)
+        
 
-pygame.init()
+    def draw(self): #Draws all the lines in the object
+        for line in self.lines:
+            pygame.draw.line(window, black,line[0].projectToPygame,line[1].projectToPygame, **self.lineInfo)
 
-a = [
-    [0,3,4],
-    [2,5,4]
+    def rotate(self,angle,axis): #Rotates the entire object by rotating each point in each line
+        newObj = []
+        for i in range(len(self.lines)):
+            newObj.append([point.rotate(angle,axis) for point in self.lines[i]])
+        self.lines = [[Vector(point) for point in line] for line in newObj]
+
+    def rotateClone(self,angle,axis): #Core of the project - generates a new object at an angle to the old one (use this to generate revolutions)
+        newObj = []
+        for i in range(len(self.lines)):
+            newObj.append([point.rotate(angle,axis) for point in self.lines[i]])
+        return obj(newObj)
+
+
+#Graph Sketching
+#This part is quite messy, I should clean it up
+
+try:
+    print("Enter a function in terms of x: ")
+    userFunc = input()
+    f = lambda x: eval(userFunc) #Actually used lambda as well I'm balling
+except:
+    print("Invalid python statement.")
+    quit()
+
+
+try: #Believe it or not, more data validation
+    print("Enter your lower bound: ")
+    userLower = eval(input())
+    print("Enter your upper bound: ")
+    userUpper = eval(input())
+    if userLower >= userUpper:
+        raise Exception
+
+    boundRange = userUpper-userLower
+    xlim = max(abs(userLower),abs(userUpper))*1.2
+    xScaleFactor = axisLength/xlim
+except:
+    print("Invalid bounds.")
+    quit()
+
+#Perhaps x+mousewheel -> Scroll only in x direction
+#Press D to return to default scroll
+try:
+    graph = []
+    yvalues = []
+    for i in range(0,100, graphResolution):
+        x1 = userLower + i*boundRange/100
+        if i == range(0,100, graphResolution)[-1]:
+            x2 = userUpper
+        else:
+            x2 = userLower + ((i+graphResolution)*boundRange)/100
+        graph.append([[x1,f(x1),0],[x2,f(x2),0]])
+        if i == 0:
+            yvalues.append(abs(f(x1)))
+        yvalues.append(abs(f(x2)))
+    ylim = max(yvalues) * 1.2
+    yScaleFactor = axisLength/ylim
+    for lines in graph:
+        for line in lines: #Getting the shape on a readable scale
+            line[0] *= xScaleFactor
+            line[1] *= yScaleFactor
+    graph = obj(graph)
+    firstCircle = [[graph.lines[0][0].rotate(num*(pi/(circleResolution/2)),"x"), graph.lines[0][0].rotate((num+1)*(pi/(circleResolution/2)),"x")] for num in range(circleResolution)]
+    lastCircle = [[graph.lines[-1][1].rotate(num*(pi/(circleResolution/2)),"x"), graph.lines[-1][1].rotate((num+1)*(pi/(circleResolution/2)),"x")] for num in range(circleResolution)]
+    firstCircle = obj(firstCircle)
+    lastCircle = obj(lastCircle) #Circles which bound the ends of the shape
+    for i in range(numberOfRevolutions):
+        graph.rotateClone(i*(pi/(numberOfRevolutions/2)),"x")
+except:
+    print("Error Occured while trying to calculate values - Maybe your graph is undefined for some value in your range?")
+    quit()
+
+
+#Objects
+
+axis = [
+    [[-1 * axisLength, 0,0],[axisLength, 0,0]],
+    [[0, -1 * axisLength,0],[0,axisLength,0]],
+    [[0, 0, -1 * axisLength],[0,0,axisLength]],
 ]
 
-b = [[2],[3],[2]]
-a = Matrix(a)
-b = Vector(b)
-c = Vector([3,4,5])
-d = Matrix([
-    [3,4,5],
-    [2,3,1],
-    [4,2,1]
-])
+axis = obj(axis,{"width": axisWidth})
 
 
-
-width, height = 800, 600
-black = 0,0,0
-white = 255,255,255
+pygame.init()
 window = pygame.display.set_mode((width, height))
-
 pygame.display.set_caption("Volumes of Revolution")
-
 running = True
 
-circles.append(Circle(Vector([400,400])))
-
-print(type(circles[0].coords))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEMOTION:
+            dist = event.rel
+            if pygame.mouse.get_pressed()[0]:
+                rotX = rotationsPerWidth * ((pi*dist[1])/width)
+                rotY = rotationsPerWidth * ((pi*dist[0])/width)
+                for object in obj.objects:
+                    object.rotate(rotX, "x")
+                    object.rotate(rotY, "y")
     window.fill((white))
-    for thing in circles:
-        pygame.draw.circle(window,black,thing.coords.vectorContents,10)
-        thing.coords = thing.coords.rotate(0.001,"anticlockwise")
-        print(type(thing.coords.vectorContents))
+    for object in obj.objects:
+        object.draw()
+
     pygame.display.flip()
+    pygame.event.pump()
 
 pygame.quit()
 sys.exit()
